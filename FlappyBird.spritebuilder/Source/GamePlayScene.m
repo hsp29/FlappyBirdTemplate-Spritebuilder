9555f9ced80330e17825e86abc5a9aa4a141bba6
hsp29 <hsp_29@yahoo.com>
2015-04-22 22:31:53 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -17,6 +17,19 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    //Increment the time since the last obstacle was added
+    timeSinceObstacle += delta;// delta is approximately 1/60th of a second
+    
+    //check to see if two seconds have passed
+    if (timeSinceObstacle >2.0f)
+    {
+        //Add a new obstacle
+        [self addObstacle];
+        
+        //then reset the timer.
+        timeSinceObstacle = 0.0f;
+    }
+    
 }
 
 // put new methods here
